b9768db94573939e03966e24ffd513a246beaf4d
vibrantwzj <250045814@qq.com>
2020-12-20 09:58:37 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 开发阶段+生产阶段。
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+  // 在当前new vue 实例中使用已注射的组件， 相当于 render:(h)=>h(App)
+})
